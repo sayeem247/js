06119f49f71fc7b6c9e3a7eb9fc11d0bafb1ad28
sayeem247 <64632813+sayeem247@users.noreply.github.com>
2020-05-04 14:07:47 +0300

    Add files via upload

---
+++ clock2.js
@@ -0,0 +1,2 @@
+n = new Date (); if (n .getTimezoneOffset () == 0) t = n.getTime () + (7 * 60 * 60 * 1000); else t = n.getTime (); n.setTime (t); var dn = new Array ("Owner "," Monday "," Tuesday "," Wednesday "," Thursday "," Friday "," Saturday "); d = n.getDay (); m = n.getMonth () + 1; y = n.getFullYear ()
+document.write (dn [d] + "," + (n.getDate () <10? "0": "") + n.getDate () + "/" + (( m <10? "0": "") + m + "/" + y)
